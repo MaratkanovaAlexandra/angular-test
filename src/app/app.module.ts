dcc8152cb0943013c94480f33c7fbb6c9872a97b
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-07-17 12:43:10 +0400

    init: angular test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { ButtonComponent } from './component/button/button.component';
+import { TaskListComponent } from './component/task-list/task-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  title:string = "Task Tracker"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onBtnClick() {
+    console.log("btn clicked");
+  }
+}
